eb97c49d28f2d7e18b939d04e57a05d0796f9cbc
ighing20 <ighing20@gmail.com>
2020-03-13 12:51:05 -0500

    add repository to ClienteService

--- soap-ws-ighing/src/com/ighing/service/ClienteService.java
+++ soap-ws-ighing/src/com/ighing/service/ClienteService.java
@@ -1,14 +1,18 @@
 package com.ighing.service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 import com.ighing.entity.Cliente;
+import com.ighing.repository.ClienteRepository;
 
 @WebService  
 public class ClienteService {
+	
+	ClienteRepository repository = new ClienteRepository(new HashMap<>());
 
 	@WebMethod
 	public String crearCliente() {
