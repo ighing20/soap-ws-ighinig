a909ece577e41cad3e225e157e7afe1a5ac752b5
emmanuel-sr <e.sirvasrueda@gmail.com>
2020-04-02 09:55:46 -0500

    change default name to webservice method result and param

--- soap-ws-ighing/src/com/ighing/service/ClienteService.java
+++ soap-ws-ighing/src/com/ighing/service/ClienteService.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 import com.ighing.entity.Cliente;
@@ -16,14 +18,16 @@ public class ClienteService {
 	ClienteRepository repository = new ClienteRepository(new HashMap<>());
 
 	@WebMethod
-	public String crearCliente(Cliente cliente) {
+	@WebResult(name = "resultado")
+	public String crearCliente(@WebParam(name = "cliente") Cliente cliente) {
 		repository.put(cliente);
-		String mensaje = "Ya se ingreso al cliente " + cliente.getNombre() + "con su numero " + cliente.getTelefono()
+		String mensaje = "Ya se ingreso al cliente " + cliente.getNombre() + " con su numero " + cliente.getTelefono()
 				+ " y direccion " + cliente.getDireccion() + ".";
 		return mensaje;
 	}
 
 	@WebMethod
+	@WebResult(name = "cliente")
 	public List<Cliente> consultarCliente() {
 		return new ArrayList<>(repository.all().values());
 	}
